f060e8d3e791d227ad99de103da3d39d7507b2cb
matthijs <mmjsvandermeijden@gmail.com>
2018-08-02 22:09:15 +0200

    added more lambda's

--- fileparser.js
+++ fileparser.js
@@ -3,20 +3,37 @@ const AWS = require('aws-sdk'); // We need to initiate the AWS service by requir
 const S3 = new AWS.S3(); // to be able to call S3 services
 
 const NAME_PREFIX= process.env.prefix;
-
+const dynamo = new AWS.DynamoDB.DocumentClient();
 exports.handler = async function(event,context, callback){
     console.log("We've received an event! :");
     console.log(JSON.stringify(event));
 
     const csvFile = await retrieveFileFromS3(event.Records[0].s3.object.key);
-    let parsedFile = Papa.parse(uploadedFile.Body.toString(), {
+    let parsedFile = parser.parse(csvFile.Body.toString(), {
         header: true
     });
 
     console.log("Successfully marshaled the data to JSON!");
     console.log(JSON.stringify(parsedFile.data));
 
-    callback(null,"hello from lambda")
+    parsedFile.data.forEach(function(parsedObject){
+        console.log(" saving record!");
+        parsedObject.orderId = parseInt(parsedObject.orderId);
+        parsedObject.itemId = parseInt(parsedObject.itemId);
+        parsedObject.amount = parseInt(parsedObject.amount);
+
+        const params ={
+            TableName: `${NAME_PREFIX}_petstore`,
+            Item: parsedObject
+        }
+
+        dynamo.put(params).promise()
+            .then(()=>{console.log("Saved item!")})
+            .catch((err)=>{console.log("Failed to save!", err)});
+    });
+
+    callback(null,"Done!");
+
 };
 
 
@@ -24,12 +41,12 @@ exports.handler = async function(event,context, callback){
     This function will invoke the
     S3 bucket and retrieve the actual file to work with inside the lambda.
  */
-function retrieveFile(fileName) {
+function retrieveFileFromS3(fileName) {
 
     const params = {
-        Bucket: `${NAME_PREFIX}.course-petstore`,
+        Bucket: `${NAME_PREFIX}.course`,
         Key: fileName
     };
     console.log(`S3 Call initiated. Trying to retrieve file: ${params.Bucket}/${params.Key}`);
-    return s3.getObject(params).promise();
+    return S3.getObject(params).promise();
 }
---
+++ getorders.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk'); // We need to initiate the AWS service by requiring the SDK
+const NAME_PREFIX= process.env.prefix;
+const dynamo = new AWS.DynamoDB.DocumentClient();
+exports.handler = async function(event,context, callback){
+    const params = {
+        TableName: `${NAME_PREFIX}_petstore`
+    };
+
+    let results = await dynamo.scan(params).promise();
+
+    let response = {
+        statusCode: 200,
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Cache-Control": "no-cache"
+        },
+        body: JSON.stringify(results.Items),
+        isBase64Encoded: false
+    };
+    callback(null,response);
+
+};
